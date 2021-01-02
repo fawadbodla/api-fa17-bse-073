267b28bf71a5505a7caa150cf038d4c094da6eee
Mohammad_Ammar <mohammadammarkhan2@gmail.com>
2021-01-02 16:13:41 +0500

    aa

---
+++ models/product.js
@@ -0,0 +1,28 @@
+var mongoose = require("mongoose");
+const joi = require('@hapi/joi');
+var productschema = mongoose.Schema({
+    name : String,
+    price : Number,
+    pid : Number,
+    Link : String,
+    category : String,
+    details : String,
+});
+
+var productmodel = mongoose.model("products",productschema);
+
+function validateproduct(data)
+{
+    const schema = joi.object({
+        name : joi.string().min(3).max(100).required(),
+        price : joi.number().min(0).required(),
+        pid : joi.number().min(0).required(),
+        Link : joi.string().required(),
+        category : joi.string().required(),
+        details : joi.string().required(),
+    });
+    return schema.validate(data);
+}
+
+module.exports.Product=productmodel;
+module.exports.validate = validateproduct;
